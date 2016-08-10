038ddb260069c4de21ee724b77eb7c547493a7ad
Hélder José <helderjosex@gmail.com>
2016-08-10 20:29:18 -0300

    Criado ambiente virtualenv

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.http import Http404
-from django.template.loader import get_template
+# from django.template.loader import get_template
 # from django.template import Context
 from django.shortcuts import render
 import datetime
