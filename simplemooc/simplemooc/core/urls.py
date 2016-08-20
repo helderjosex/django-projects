f732fcf23d507e7416e8e1e8da217565dfb6a796
Hélder José <helderjosex@gmail.com>
2016-08-20 15:13:09 -0300

    Implementado sistema de templates

---
+++ simplemooc/simplemooc/core/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from simplemooc.core import views as core_views
+
+urlpatterns = [
+    url(r'^$', core_views.home, name='home'),
+    url(r'^contato/', core_views.contact, name='contact'),
+
+]
