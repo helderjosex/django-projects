2f0ac05c17eb1ad9a0bbb0ba136a9e786fd15c6c
Hélder José <helderjosex@gmail.com>
2016-08-28 19:36:55 -0300

    Finalizado cadastro de usuario e autenticacao

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
+from simplemooc.accounts import views as accounts_views
 
 
 urlpatterns = [
     url(r'^entrar/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^sair/$', logout, {'next_page': 'core:home'}, name='logout'),
+    url(r'^cadastre-se/$', accounts_views.register, name='register'),
 
 ]
--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.conf import settings
+from .forms import RegisterForm
 
-# Create your views here.
+def register(request):
+    template_name = 'register.html'
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user = authenticate(username=user.username, password=form.cleaned_data['password1'])
+            login(request, user)
+            return redirect('core:home')
+    else:
+     form = RegisterForm()
+    context = {
+        'form': form
+    }
+    return render(request, template_name, context)
