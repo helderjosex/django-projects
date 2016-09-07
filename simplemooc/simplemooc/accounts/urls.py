8d9123915cb4382c0c237ade29c794e98d8a5c43
Hélder José <helderjosex@gmail.com>
2016-09-07 15:12:52 -0300

    Implementado painel do usuário

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -4,8 +4,11 @@ from simplemooc.accounts import views as accounts_views
 
 
 urlpatterns = [
+    url(r'^$', accounts_views.dashboard, name='dashboard'),
     url(r'^entrar/$', login, {'template_name': 'login.html'}, name='login'),
     url(r'^sair/$', logout, {'next_page': 'core:home'}, name='logout'),
     url(r'^cadastre-se/$', accounts_views.register, name='register'),
+    url(r'^editar/$', accounts_views.edit, name='edit'),
+    url(r'^editar-senha/$', accounts_views.edit_password, name='edit_password'),
 
 ]
